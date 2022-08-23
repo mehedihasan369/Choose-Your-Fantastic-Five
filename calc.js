9e5cf0d056956d641d21c2e3ffd5eed3ab88a6ed
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-08-23 21:09:44 +0600

    js

--- calc.js
+++ calc.js
@@ -3,6 +3,7 @@ const cartArray = [];
 function display(cartPlayer) {
     // console.log(cartPlayer);
     const myList = document.getElementById("my-list");
+    myList.innerText =""  ;
 
     for (let i=0; i< cartPlayer.length ; i++){
         const selected =cartArray[i];
@@ -14,20 +15,74 @@ function display(cartPlayer) {
 
 }
 
-
+// document.getElementById('calculate').addEventListener('click', function(){
 
 function addToCart(element) {
     element.disabled = true ;
     console.log(element.parentNode.children)
     const playerName = element.parentNode.children[0].innerText;
 
-    cartArray.push(playerName)
-    
+   cartArray.push(playerName)
+
+   
+
     console.log(cartArray)
+     
+    
+    display(cartArray) ;
+    
+    
+    const arrayLength =  cartArray.length
 
-    const playerQuantity = cartArray.length;
 
-    console.log(playerQuantity)
+    
+///****Cost Calculation********** */
 
-    display(cartArray)
+
+
+
+document.getElementById('calculate').addEventListener('click', function(){
+    const perPlayer = document.getElementById('per-player');
+    const newPlayerString = perPlayer.value;
+    const newPlayerAmount = parseFloat(newPlayerString);
+
+    
+    const playerExpenses = newPlayerAmount * arrayLength;
+
+    // console.log(playerExpenses)
+
+    const depositTotalElement = document.getElementById('player-expenses');
+    depositTotalElement.innerText =  playerExpenses;
+
+   //////////////***************************** */
+
+   const manager = document.getElementById('manager');
+   const managerString = manager.value;
+   const managerFloat = parseFloat(managerString);
+   /////****************************** */
+
+   const coach = document.getElementById('coach');
+   const coachString = coach.value;
+   const coachFloat = parseFloat(coachString);
+
+   
+  document.getElementById('calculate-total').addEventListener('click', function(){
+
+    const total = managerFloat +coachFloat + playerExpenses ;
+
+    //////////////////////////
+    const Total = document.getElementById('total');
+    Total.innerText =  total;
+
+  })
+
+
+   console.log(playerExpenses);
+})
+
+
+
 }
+
+
+
